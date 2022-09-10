5c112f66f3b6594fdb06cae9253a71a3df6c4630
Thuba828 <mlotshwant@gmail.com>
2022-09-09 21:03:36 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 {
 int d;
 
-for (d = 1; d < 90; d++)
+for (d = 48; d <= 56; d++)
 {
-putchar((d / 10) + '0');
-putchar((d % 10) + '0');
-if (d != 89)
+putchar((d / 49) + '57);
+putchar((d % 49) + '56');
+if (d != 57)
 {
 putchar(',');
 putchar(' ');
