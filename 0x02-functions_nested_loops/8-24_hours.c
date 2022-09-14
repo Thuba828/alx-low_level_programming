85b726de2ccccfade9fb48fb1fe5eebcd7f60919
Thuba828 <mlotshwant@gmail.com>
2022-09-14 04:49:14 -0700

    Every minute of everyday

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,26 +5,19 @@
  * Return: 0
  */
 void jack_bauer(void)
-{	
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+{
+	int min, hr;
 
-	while (hours <= 23)
+	for(hr = 0; hr <= 23; hr++)
 	{
-	while (minutes < = 59)
-	{
-av		
-		hours_remainder = hours % 10;
-		_putchar(hours/10 + '0');
-		_putchar(hour_remainder + '0')
-		_putchar(':');
-		_putchar(minutes / 10 + '0');
-		minutes++;
-		_putchar('\n');
-	}
-	hours++;
-	minutes = 0;
+		for(min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
 	}
 }
