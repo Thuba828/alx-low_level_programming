c55543f56d619867f4bd5c901281d76fb5c16df9
Thuba828 <mlotshwant@gmail.com>
2022-09-15 11:17:25 -0700

    Printing out numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_numbers - prints out all the numbers
+*starts from 0 to 9
+*
+*Return: returns nothing
+*/
+
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
