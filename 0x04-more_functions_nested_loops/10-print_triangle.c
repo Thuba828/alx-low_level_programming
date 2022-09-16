c7c106492346b228303f5b5d6c318a796936ffb0
Thuba828 <mlotshwant@gmail.com>
2022-09-15 17:08:09 -0700

    Printing a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,21 +19,17 @@ void print_triangle(int size)
 			{
 				putchar(' ');
 			}
-			
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-					putchar('#');
+				putchar('#');
 			}
-
 			if (inc1 == size)
 			{
 				continue;
 			}
-
 			putchar('\n');
 		}
-
 	}
-	putchar ('\n');
+	putchar('\n');
 }
 
