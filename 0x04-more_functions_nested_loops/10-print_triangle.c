e2f94820abbcb369f9a02fc27d40f41fefc357d3
Thuba828 <mlotshwant@gmail.com>
2022-09-15 21:07:24 -0700

    Triangle drawing

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,23 +9,26 @@
 
 void print_triangle(int size)
 {
-	if (size <= 0)
+	int inc1, inc2;
+
+	if (size > 0)
 	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
-		for (i = 1; i <= size; i++)
+		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for (j = i; j < size; j++)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
-				_putchar(' ');
+				putchar(' ');
 			}
-			for (j = 1; j <= i; j++)
+			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-				_putchar('#');
+				putchar('#');
 			}
-			_putchar('\n');
+			if (inc1 == size)
+			{
+				continue;
+			}
+			putchar('\n');
 		}
 	}
+	putchar('\n');
 }
