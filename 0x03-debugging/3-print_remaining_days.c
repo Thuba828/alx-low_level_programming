ef26a3ee971dd7d3891fd2567dfb2e22c198c94c
Thuba828 <mlotshwant@gmail.com>
2022-09-15 19:44:12 -0700

    Main C file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		printf("Day of the year: %d\n", day);
+i		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	} else
 	{
