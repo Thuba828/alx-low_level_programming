030b9667953756b5604e53ac99a52736c6ddff32
Thuba828 <mlotshwant@gmail.com>
2022-09-19 14:58:42 -0700

    Replacing with 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 
 
 int _putchar(char c);
-void reset_to_98(&n);
-void swap_int(&a, &b);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
