f42573ec82fc479507a5d26a205f414169a957c8
Thuba828 <mlotshwant@gmail.com>
2022-09-22 22:45:14 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,78 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers strored in string
- * The number will always return a 0 or a positive number
- * @n1: first number added
- * @n2: second number added
- * @r: store results, @size_r: size of bufffer 
- * Return: returns a pointer
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+void rev_string(char *n)
 {
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
 }
 
 /**
- * add_strings - Adds the numbers stored in two strings
- * @n1: The string containing the first
- * number to be added.
- * @n2: string containing the second number
- * @r: The buffer
- * @r_index: the current index
- * Return: if r can store the sum - a pointer to the result, if r cannot store the sum - 0.
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-{
-	int num, tens = 0;
 
-	for (; *n1 && *n2; n1--, n2--, r_index--)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	for (; *n1; n1--; r_index++)
-	{
-		num = *(n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-	       tens = num / 10;
-	}
-	for (; *n2; n2--; r_index--)
-	{
-	num = (*n2 - '0') + tens;
-	*(r + r_index) = (num % 10) + '0';
-	tens = num / 10;
-	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }	
